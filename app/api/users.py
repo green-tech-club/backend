0410d5407e9a16439e20b6e375fc8586bed9fb79
Oualid Ezzoubeidi <siszko37@gmail.com>
2023-03-19 22:00:18 +0100

    added signup and login endpoints + adding passwords in users schema and hashing before adding them to db

--- app/api/users.py
+++ app/api/users.py
@@ -1,11 +1,13 @@
 from typing import List, Optional
-from fastapi import Body, HTTPException, status
+from fastapi import Body, HTTPException, status, Request
 from fastapi import APIRouter
 from fastapi.responses import Response, JSONResponse
 from fastapi.encoders import jsonable_encoder
 
 from models.user import UserModel, UpdateUserModel
 from db.mongo import db
+
+import uuid
 
 user_routes = APIRouter()
 
@@ -17,11 +19,52 @@ async def create_new_user(user_data: UserModel = Body(...)):
     return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_user)
 
 
-@user_routes.get("/users", response_description="List all (first 1000 actually) users", response_model=List[UserModel])
+@user_routes.post("/signup", response_description="New users signing up")
+async def create_user(request: Request):
+    try:
+        user = await UserModel.find_by_email(request._query_params['email'])
+        if (user != None):
+            return {'error: This email already exists!'}
+        
+        new_user = UserModel(name=request._query_params['name'], email=request._query_params['email'], password=request._query_params['password'])   
+        new_user.save_new_user()
+
+        return JSONResponse(status_code=201, 
+                            content={'user': {
+                                    'name': new_user.name,
+                                    'email': new_user.email
+                            }})
+    except Exception as e:
+        return {'Error: Failed to create a new user'}
+
+
+@user_routes.post("/login", response_description="Endpoint for login for existing users")
+async def login(reqest: Request):
+    print('login')
+    try:
+        user = await UserModel.find_by_email(reqest._query_params['email'])
+        if (user): 
+            # check password
+            is_pass_valid = await UserModel.is_password_correct(reqest._query_params['password'].encode('utf-8'), user['password'].encode('utf-8'))
+            if (is_pass_valid):
+                # generate token
+                token = str(uuid.uuid4())
+                # login succesfull
+                return JSONResponse(status_code=200, 
+                content={'message': 'successfully logged in', 'token': token})
+            else:
+                return JSONResponse(status_code=403, content={'message': 'entered password is wrong'})
+        else:
+            return {'Error: user does not exist.'}
+    except Exception as e:
+        print(e)
+        return {'Error: failed to login'}
+
+
+@user_routes.get("/list/users", response_description="List all (first 1000 actually) users", response_model=List[UserModel])
 async def list_users():
     users = await db["users"].find().to_list(1000)
     return users
-
 
 @user_routes.get("/users/{id}", response_description="Get a single user", response_model=UserModel)
 async def get_user_by_id(id: str):
--- app/models/user.py
+++ app/models/user.py
@@ -4,10 +4,15 @@ from bson import ObjectId
 
 from .object import PyObjectId
 
+from db.mongo import db
+import bcrypt
+
+
 class UserModel(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     name: str = Field(...)
     email: EmailStr = Field(...)
+    password: str = Field(...)
 
     class Config:
         allow_population_by_field_name = True
@@ -17,9 +22,29 @@ class UserModel(BaseModel):
             "example": {
                 "name": "Jane Doe",
                 "email": "jdoe@example.com",
+                "password": "$2b$12$PQb5mqwNlQhb5GuceK2BROIjRa.ORdKxUesB4gUnx.TRvYMF/z906"
             }
         }
 
+    def find_by_email(email):
+        user = db['users'].find_one({'email': email})
+        return user
+    
+    
+    def save_new_user(self):
+        self.create_hashed_password()
+        db['users'].insert_one(dict(vars(self)))
+
+
+    def create_hashed_password(self):
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(self.password.encode('utf-8'), salt)
+        self.password = hashed_password.decode('utf-8')
+
+
+    async def is_password_correct(entered_password, user_password):
+        is_valid = bcrypt.checkpw(entered_password, user_password)
+        return is_valid
 
 class UpdateUserModel(BaseModel):
     name: Optional[str]
