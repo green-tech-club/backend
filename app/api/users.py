157711751cf22d7683fd5605461044700f1fbe65
Csaba Balogh <csaba.balogh.92@gmail.com>
2023-04-05 18:40:33 +0200

    fixed typo

--- app/api/users.py
+++ app/api/users.py
@@ -3,14 +3,14 @@ from fastapi import APIRouter, Depends
 from app.models.token import AccessToken
 from app.db import db
 from app.models.user import User, UserRead
-from app.users import UserManager, current_active_user
+from app.users import current_active_user
 from app.db import get_access_token_db, get_user_db
 
 user_routes = APIRouter()
 
 @user_routes.get("/list", tags=["users"], response_model=list[User], response_description="List of users")
 async def list_users(quantity: int = 100) -> List[User]:
-    ls = await UserManager.find_all().to_list(quantity)
+    ls = await User.find_all().to_list(quantity)
     return ls
 
 
