85a586961f9bcdf2a3bb0bf8b635edf23aa7fd1f
Csaba Balogh <csaba.balogh.92@gmail.com>
2023-04-21 23:26:07 +0200

    cosmetics

--- app/models/user.py
+++ app/models/user.py
@@ -3,7 +3,6 @@ from fastapi_users import schemas
 from fastapi_users.db import BeanieBaseUser
 
 class User(BeanieBaseUser[PydanticObjectId]):
-    # hashed_password: str = None
     name: str = None
     role: str = None
 
